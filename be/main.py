ca13b6610a40d2a77adee976ccf075fd4e488beb
Quang <quangngo7821@gmail.com>
2025-03-12 15:29:44 +0700

    delete console log debug

--- be/main.py
+++ be/main.py
@@ -168,8 +168,8 @@ def compare():
 
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000)
-    
 # if __name__ == '__main__':
-#     app.run(debug=False, host="0.0.0.0", port=10000)
+#     app.run(debug=True, host="0.0.0.0", port=5000)
+    
+if __name__ == '__main__':
+    app.run(debug=False, host="0.0.0.0", port=10000)
